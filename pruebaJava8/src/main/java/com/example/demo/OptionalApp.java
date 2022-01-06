6a3ee61da0a546efed5944c7aa7db65c4722e0d0
Arthur <arturormzh1223@gmail.com>
2022-01-05 23:34:49 -0600

    Manejo y uso de wrapper Optional

---
+++ pruebaJava8/src/main/java/com/example/demo/OptionalApp.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+public class OptionalApp {
+	
+	public void probar(String valor) {
+//		System.out.println(valor.contains("art"));
+		
+		try {
+			Optional op = Optional.empty();
+			op.get();
+		} catch (Exception e) {
+			System.out.println("No hay elemento");
+		}
+	}
+	
+	public void orElse(String valor) {
+		Optional<String> op = Optional.ofNullable(valor);
+		String x = op.orElse("Predeterminado");
+		
+		System.out.println(x);
+	}
+
+	public void orElseThrow(String valor) {
+		Optional<String> op = Optional.ofNullable(valor);
+		op.orElseThrow(NumberFormatException::new);
+	}
+	
+	public void isPresent(String valor) {
+		Optional<String> op = Optional.ofNullable(valor);
+		System.out.println(op.isPresent());
+	}
+
+	public static void main(String[] args) {
+		OptionalApp app = new OptionalApp();
+//		app.probar("art");
+//		app.orElse(null);
+//		app.orElseThrow(null);
+		app.isPresent(null);
+	}
+	
+}
